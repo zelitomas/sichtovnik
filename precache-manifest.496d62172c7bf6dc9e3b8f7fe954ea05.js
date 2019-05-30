4b5a948c07293a1c7634458556dc4569210de8cf
Tomáš Zelina <jmenujise@tomaszelina.cz>
2019-05-30 23:54:55 +0200

    Updates

---
+++ precache-manifest.496d62172c7bf6dc9e3b8f7fe954ea05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2294161cf3a8a0bfe2cf3c06e60d99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "643263eaacb29313068b",
+    "url": "./static/css/main.3907b3dd.chunk.css"
+  },
+  {
+    "revision": "f86fbee91065457cb4c4",
+    "url": "./static/js/2.f8b2c143.chunk.js"
+  },
+  {
+    "revision": "643263eaacb29313068b",
+    "url": "./static/js/main.c2848813.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f73e1854fdadbc34433bc74d9e5abae8",
+    "url": "./static/media/wavingHand.f73e1854.svg"
+  }
+]);
