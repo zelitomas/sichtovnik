02f39c4179d322a82b46c732cf32ca59846f8388
Tomáš Zelina <zelitomas@gmail.com>
2020-04-23 22:52:42 +0200

    :wastebasket: Removed deprecated shifts

--- src/defaults.js
+++ src/defaults.js
@@ -1,65 +1,42 @@
 let predefined;
 
-if (process.env.REACT_APP_MODE === 'flat') {
-    let flatWork = {colors: [], names:[]};
 
-    for(let j of ["KŠ", "KO", "-", "OB", "KU", "PO"]){
-        for(let i = 0; i < 7; i++){
-            flatWork.names.push(j);
-        }
-    }
-    for(let j of ["#2c3e50", "#7f8c8d", "#27ae60",  "#c0392b", "#2980b9", "#e67e22"]){
-        for(let i = 0; i < 7; i++){
-            flatWork.colors.push(j);
-        }
-    }
+const commonNames = {
+    "3shift": {
+        colors: ["#27ae60", "#27ae60", "#e67e22", "#e67e22", "#2c3e50", "#2c3e50", "#2980b9", "#2980b9"],
+        names: ["R", "R", "O", "O", "N", "N", "-", "-"]
+    },
+    "3shiftAlternative": {
+        colors: ["#27ae60", "#27ae60", "#2c3e50", "#2c3e50", "#e67e22", "#e67e22", "#2980b9", "#2980b9"],
+        names: ["R", "R", "N", "N", "O", "O",  "-", "-"]
+    },
+    "3shift-all": {
+        colors: ["#27ae60", "#27ae60", "#e67e22", "#e67e22", "#2c3e50", "#2c3e50", "#2980b9", "#2980b9"],
+        names: ["ABC", "ABC", "DAB", "DAB", "CDA", "CDA", "BCD", "BCD"]
+    },
+    "3shiftWithWeekends": {
+        colors: ["#27ae60", "#27ae60", "#27ae60", "#27ae60", "#27ae60", "#2980b9",  "#2980b9", "#e67e22", "#e67e22", "#2c3e50", "#2c3e50", "#2c3e50", "#2980b9", "#2980b9", "#2c3e50", "#2c3e50", "#e67e22", "#e67e22", "#e67e22", "#2980b9",  "#2980b9"],
+        names: ["R", "R", "R", "R", "R", "-",  "-", "O", "O", "N", "N", "N", "-", "-", "N", "N", "O", "O", "O", "-",  "-"]
+    },
 
-     predefined = [
-        { name: "Lukáš", days: 7*6, offset: 3 + 7, scheme: flatWork },
-        { name: "Tomáš", days: 7*6, offset: 3 + 2 * 7, scheme: flatWork },
-        { name: "David", days: 7*6, offset: 3 + 3 * 7, scheme: flatWork },
-        { name: "Dominik", days: 7*6, offset: 3 + 4 * 7, scheme: flatWork },
-        { name: "Iza", days: 7*6, offset: 3 + 5 * 7, scheme: flatWork },
-        { name: "Jakub", days: 7*6, offset: 3 + 6 * 7, scheme: flatWork },
-    ]
-
-} else {
-    const commonNames = {
-        "3shift": {
-            colors: ["#27ae60", "#27ae60", "#e67e22", "#e67e22", "#2c3e50", "#2c3e50", "#2980b9", "#2980b9"],
-            names: ["R", "R", "O", "O", "N", "N", "-", "-"]
-        },
-        "3shiftAlternative": {
-            colors: ["#27ae60", "#27ae60", "#2c3e50", "#2c3e50", "#e67e22", "#e67e22", "#2980b9", "#2980b9"],
-            names: ["R", "R", "N", "N", "O", "O",  "-", "-"]
-        },
-        "3shift-all": {
-            colors: ["#27ae60", "#27ae60", "#e67e22", "#e67e22", "#2c3e50", "#2c3e50", "#2980b9", "#2980b9"],
-            names: ["ABC", "ABC", "DAB", "DAB", "CDA", "CDA", "BCD", "BCD"]
-        },
-        "3shiftWithWeekens": {
-            colors: ["#27ae60", "#27ae60", "#27ae60", "#27ae60", "#27ae60", "#2980b9",  "#2980b9", "#e67e22", "#e67e22", "#2c3e50", "#2c3e50", "#2c3e50", "#2980b9", "#2980b9", "#2c3e50", "#2c3e50", "#e67e22", "#e67e22", "#e67e22", "#2980b9",  "#2980b9"],
-            names: ["R", "R", "R", "R", "R", "-",  "-", "O", "O", "N", "N", "N", "-", "-", "N", "N", "O", "O", "O", "-",  "-"]
-        },
-
-    };
+};
 
 
 
-    predefined = [
-        { name: "Železárny A", days: 8, offset: 1, scheme: commonNames["3shift"] },
-        { name: "Železárny B", days: 8, offset: 3, scheme: commonNames["3shift"] },
-        { name: "Železárny C", days: 8, offset: 5, scheme: commonNames["3shift"] },
-        { name: "Železárny D", days: 8, offset: 7, scheme: commonNames["3shift"] },
-        { name: "Železárny A Bohumín", days: 8, offset: 1, scheme: commonNames["3shiftAlternative"] },
-        { name: "Železárny B Bohumín", days: 8, offset: 3, scheme: commonNames["3shiftAlternative"] },
-        { name: "Železárny C Bohumín", days: 8, offset: 5, scheme: commonNames["3shiftAlternative"] },
-        { name: "Železárny D Bohumín", days: 8, offset: 7, scheme: commonNames["3shiftAlternative"] },
-        { name: "Železárny A Ostrava", days: 21, offset: 17, scheme: commonNames["3shiftWithWeekens"] },
-        { name: "Železárny B Ostrava", days: 21, offset: 3, scheme: commonNames["3shiftWithWeekens"] },
-        { name: "Železárny C Ostrava", days: 21, offset: 10, scheme: commonNames["3shiftWithWeekens"] },
-        /*{ name: "Železárny souhrn (zatím nefunkční!)", days: 8, offset: 1, scheme: commonNames["3shift-all"] },*/
-    ];
-}
+predefined = [
+    { name: "Železárny A", days: 8, offset: 1, scheme: commonNames["3shift"] },
+    { name: "Železárny B", days: 8, offset: 3, scheme: commonNames["3shift"] },
+    { name: "Železárny C", days: 8, offset: 5, scheme: commonNames["3shift"] },
+    { name: "Železárny D", days: 8, offset: 7, scheme: commonNames["3shift"] },
+    { name: "Železárny nové A", days: 8, offset: 1, scheme: commonNames["3shiftAlternative"] },
+    { name: "Železárny nové B", days: 8, offset: 3, scheme: commonNames["3shiftAlternative"] },
+    { name: "Železárny nové C", days: 8, offset: 5, scheme: commonNames["3shiftAlternative"] },
+    { name: "Železárny nové D", days: 8, offset: 7, scheme: commonNames["3shiftAlternative"] },
+    { name: "Železárny A Ostrava", days: 21, offset: 17, scheme: commonNames["3shiftWithWeekends"] },
+    { name: "Železárny B Ostrava", days: 21, offset: 3, scheme: commonNames["3shiftWithWeekends"] },
+    { name: "Železárny C Ostrava", days: 21, offset: 10, scheme: commonNames["3shiftWithWeekends"] },
+    /*{ name: "Železárny souhrn (zatím nefunkční!)", days: 8, offset: 1, scheme: commonNames["3shift-all"] },*/
+];
+
 
 export default predefined;
